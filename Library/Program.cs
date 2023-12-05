11ed79a9896d7ea2a68c91c0be3485c134e1a883
Emily Flynn <emily.flynn@softwire.com>
2023-12-05 14:27:05 +0000

    Add swagger

--- Library/Program.cs
+++ Library/Program.cs
@@ -16,16 +16,17 @@ builder.Services.AddTransient<ILoanRepo, LoanRepo>();
 
 builder.Services.AddDbContext<LibraryContext>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 DbPopulator.PopulateDatabase();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseCors(x => x
@@ -33,7 +34,8 @@ app.UseCors(x => x
     .AllowAnyHeader()
     .AllowAnyMethod());
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
